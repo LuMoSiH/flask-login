c3acd0ae9bee3cf64ab3d333903e0a5261d053e6
Luan Morengui <luan.morengui@gmail.com>
2022-04-21 13:24:49 -0300

    Login functionality + session control

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # flask-login
 [Python]
+
+Basic python example login using flask + sqlite3 + session control.
+Check the "requirements.txt" file
+
--- main.py
+++ main.py
@@ -1,76 +1,87 @@
 from flask import Flask, render_template, redirect, url_for, session, flash, request
 from flask_session import Session
 from datetime import timedelta
-from werkzeug.security import generate_password_hash, check_password_hash
-import pandas as pd
+import sqlite3
+import random
 
+# Config App
 app = Flask(__name__, static_folder="./static", template_folder="templates")
-app.secret_key = "U2VjcmV0a2V5THVhbk1vcmVuZ3Vp"
+app.secret_key = "U2VjcmV0a2V5THVhbk1vcmVuZ3Vp"#<<<<< INSERT YOUR SECRET KEY
 # Config session
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+# Time Session
 app.permanent_session_lifetime = timedelta(days=7)
 Session(app)
 
 
+def get_db_connection():
+    conn = sqlite3.connect("db.db", check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # Session validation
+    if not session.get("login"):
+        return redirect("/login")
+    # Random image welcome
+    welcome = [
+        'https://media2.giphy.com/media/dZXzmKGKNiJtDxuwGg/giphy.gif?cid=ecf05e475nnedi7zay7w7brr9rmj112tyqdsqfoyy276jejm&rid=giphy.gif&ct=g',
+        'https://media2.giphy.com/media/EKrFksrzxQxlb5ahiq/giphy.gif?cid=ecf05e475nnedi7zay7w7brr9rmj112tyqdsqfoyy276jejm&rid=giphy.gif&ct=g',
+        'https://media4.giphy.com/media/l4JyOCNEfXvVYEqB2/giphy.gif?cid=ecf05e475nnedi7zay7w7brr9rmj112tyqdsqfoyy276jejm&rid=giphy.gif&ct=g'
+    ]
+
+    return render_template("index.html", img=random.choice(welcome))
 
 
 @app.route("/login")
 def login():
+    # Session validation
+    if session.get("login"):
+        return redirect("/")
     return render_template("login.html")
-
-
-@app.route("/SignUp")
-def SignUp():
-    return render_template("SignUp.html")
-
-
-@app.route("/register", methods=["POST"])
-def register():
-    login = request.form.get("login")
-    password = generate_password_hash(request.form.get("password"))
-    # FORM VALIDATION
-    if not login:
-        flash("Missing login", "secondary")
-        return redirect("/SignUp")
-
-    if len(request.form.get("password")) == 0:
-        flash("Missing password", "secondary")
-        return redirect("/SignUp")
-
-    users.loc[len(users)] = [login, password]
-
-    #print(users.index(login))
-
-
-    return redirect("/")
 
 
 @app.route("/logon", methods=["POST"])
 def logon():
-    login = request.form.get("cpf")
+    login = request.form.get("login")
     password = request.form.get("password")
     remember = request.form.get("remember")
     # Form Validation
     if not login:
-        flash("Missing CPF", "secondary")
+        flash("Missing login", "secondary")
         return redirect("/login")
 
     if not password:
         flash("Missing password", "secondary")
         return redirect("/login")
 
-    return redirect(url_for("/"))
+    # Verify if account exist
+    conn = get_db_connection()
+    account = conn.execute(f"SELECT * FROM users where login='{login}'").fetchone()
+    conn.close()
+
+    if account and password == account['password']:
+        # Session On or Off - 7 Days
+        match remember:
+            case "None":
+                session.permanent = False
+            case "on":
+                session.permanent = True
+
+        session["login"] = account['login']  # login
+
+        return redirect("/")
+
+    flash("Invalid credentials", "secondary")
+    return redirect("/login")
 
 
 @app.route("/logout")
 def logout():
     session.clear()
-    return redirect("/")
+    return redirect("/login")
 
 
 if __name__ == "__main__":
